dddfd4e2ebd9c92557fe7c58f140bbb79c2f444f
david <davidf1212@gmail.com>
2015-03-07 20:12:31 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app     = express();
+var http    = require('http').Server(app);
+var io      = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function (socket) {
+});
+
+http.listen(process.env.PORT || 3000, function() {
+	console.log('listening on *:3000');
+});
