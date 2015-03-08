d8715b3ee014fb3a3d48d183195aecae925f9069
baieric <ebai@uwaterloo.ca>
2015-03-07 23:10:19 -0500

    adding reveal roles view

--- public/js/game.js
+++ public/js/game.js
@@ -1,22 +1,46 @@
 var socket = io();
+
+var usernames = [];
+var myUser = {};
+
+// main divs
+var joinGameView = document.getElementById('join-game');
+var roleRevealView = document.getElementById('reveal-roles');
+
+// join-game elements
 var nameButton = document.getElementById('btn-name');
 var nameField = document.getElementById('input-name');
 var startButton = document.getElementById('btn-start');
 var userList = document.getElementById('list-users');
 
+// initial state
 startButton.disabled = true;
+roleRevealView.style.display = 'none';
 
+
+// joinGameView
+nameField.onkeypress = function(e){
+	if (!e) e = window.event;
+    var keyCode = e.keyCode || e.which;
+    if (keyCode == '13'){
+    	submitName(nameField.value);
+    }
+}
 nameButton.onclick = function() {
 	submitName(nameField.value);
 };
-
 startButton.onclick = function(){
 	socket.emit('start game');
+	joinGameView.style.display = 'none';
+	roleRevealView.style.display = 'block';
 };
+
 
 socket.on('users joined', function (users) {
 	users.forEach(function (user) {
-		var li = document.createElement("li");
+		usernames.push(user.name);
+		var li = document.createElement('li');
+		li.className = 'list-group-item';
 		li.appendChild(document.createTextNode(user.name));
 		userList.appendChild(li);
 	});
@@ -26,8 +50,27 @@ socket.on('allow game start', function(){
 	startButton.disabled = false;
 });
 
+socket.on('assign role', function(users){
+	users.forEach(function(user){
+		if(user.name == myName){
+			myUser = user;
+		}
+	});
+});
+
 function submitName( name ){
-	if (name.length > 1){
+	var nameTaken = false;
+
+	usernames.forEach(
+		function(username){
+			if(name == username){
+				nameTaken = true;
+			}
+		}
+	);
+
+	if (name.length > 1 && !nameTaken){
+		myUser.name = name;
 		socket.emit('add user', name);
 		nameButton.disabled = true;
 		nameField.disabled = true;
